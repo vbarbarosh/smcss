3cc8036f9181efcf72790caae806b9f5243d9c21
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2017-04-05 07:49:43 +0300

    add demo/hsplit

---
+++ demo/hsplit/5.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Title of the page</title>
+    <link href="../demo.css?<?php echo time() ?>" type="text/css" rel="stylesheet">
+</head>
+<body class="checkerboard">
+
+    <div class="fix tlbr vsplit rainbow">
+        <div>
+            <h2>header</h2>
+        </div>
+        <div class="fluid hsplit">
+            <div class="w200 vsplit cyan">
+                <div class="yellow">
+                    <h2>top</h2>
+                </div>
+                <div class="fluid oa">
+                    <h2>gallery</h2>
+                </div>
+                <div class="red">
+                    <h2>buttons</h2>
+                </div>
+            </div>
+            <div class="fluid silver oa">
+                <h2>middle</h2>
+                <p>Pellentesque sapien nulla, rhoncus non dictum at, dignissim at purus. Aliquam erat volutpat. Maecenas eget lectus blandit, consequat ligula non, mattis risus. Cras pulvinar vulputate ultrices. Curabitur diam augue, rutrum tincidunt mauris id, egestas convallis arcu. Aenean feugiat ullamcorper nulla, eu rhoncus sem convallis sit amet. Vestibulum scelerisque, sem eget imperdiet porta, tortor justo egestas lacus, eu iaculis erat elit id ipsum. Donec luctus ante ac ornare ultricies. Fusce dapibus, nisi sed luctus eleifend, est ipsum pharetra felis, ut pellentesque risus nisl sodales arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo at nibh commodo varius. Integer tempus vel justo sed feugiat.</p>
+                <p>Donec et ornare enim. Phasellus rutrum diam quis diam imperdiet sagittis. Fusce vel nisl sagittis, hendrerit tortor et, sollicitudin odio. Donec sodales quam eget turpis tincidunt, sodales aliquet massa porttitor. Curabitur eget gravida nibh. Cras nec urna vitae urna ullamcorper malesuada ac suscipit urna. Donec vulputate massa lorem, vel varius est iaculis id. Maecenas sollicitudin quis quam vitae viverra. Sed dictum nibh non massa suscipit, non porta elit finibus. Nam id egestas eros, sed laoreet ligula. Nunc commodo est nec ante pellentesque, at rutrum libero laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras quis lacus at lectus consequat sodales.</p>
+                <p>Morbi porttitor velit eget mi viverra, ut elementum dolor porta. Duis pellentesque, purus quis sodales ullamcorper, orci neque commodo dui, sit amet volutpat dui nulla ac ante. Proin faucibus auctor mi non consequat. Suspendisse faucibus lorem sem, a interdum magna rutrum et. Suspendisse vulputate, mauris eget elementum viverra, enim magna egestas nulla, at suscipit neque nunc sit amet nisi. Nunc et dui eros. Aliquam ut nunc dictum ante fringilla interdum. Curabitur dapibus dolor eu sapien dignissim, id ornare turpis iaculis. Nam scelerisque finibus urna quis pharetra. Donec nec felis tortor.</p>
+                <p>Suspendisse ipsum nunc, posuere eget auctor a, pretium eget tortor. Duis hendrerit augue vel est auctor semper. Pellentesque ultrices euismod risus at imperdiet. Mauris viverra lorem commodo felis facilisis suscipit. Vivamus semper ex in dolor mollis, eu fringilla tortor molestie. Praesent ut leo non nisl egestas interdum et accumsan erat. Vivamus a quam nisl. Integer ornare leo libero, non interdum ex rhoncus et.</p>
+                <p>Aenean vitae metus euismod, consectetur lacus ac, sollicitudin nulla. Aliquam non ex in est egestas congue et sit amet urna. Maecenas gravida posuere eros at luctus. Sed laoreet sem non efficitur ultrices. Maecenas efficitur pretium ultrices. Suspendisse congue turpis sapien, sit amet pulvinar lacus vulputate hendrerit. Quisque rhoncus nulla non libero ornare venenatis. Pellentesque eget laoreet ligula. Phasellus at felis et nisl iaculis aliquam. Maecenas a mollis tellus, in porta enim. Aliquam nec euismod elit. Aenean quis odio ut ante vehicula vulputate. Suspendisse non ipsum sollicitudin, eleifend purus et, sollicitudin massa.</p>
+                <p>Pellentesque sapien nulla, rhoncus non dictum at, dignissim at purus. Aliquam erat volutpat. Maecenas eget lectus blandit, consequat ligula non, mattis risus. Cras pulvinar vulputate ultrices. Curabitur diam augue, rutrum tincidunt mauris id, egestas convallis arcu. Aenean feugiat ullamcorper nulla, eu rhoncus sem convallis sit amet. Vestibulum scelerisque, sem eget imperdiet porta, tortor justo egestas lacus, eu iaculis erat elit id ipsum. Donec luctus ante ac ornare ultricies. Fusce dapibus, nisi sed luctus eleifend, est ipsum pharetra felis, ut pellentesque risus nisl sodales arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo at nibh commodo varius. Integer tempus vel justo sed feugiat.</p>
+                <p>Donec et ornare enim. Phasellus rutrum diam quis diam imperdiet sagittis. Fusce vel nisl sagittis, hendrerit tortor et, sollicitudin odio. Donec sodales quam eget turpis tincidunt, sodales aliquet massa porttitor. Curabitur eget gravida nibh. Cras nec urna vitae urna ullamcorper malesuada ac suscipit urna. Donec vulputate massa lorem, vel varius est iaculis id. Maecenas sollicitudin quis quam vitae viverra. Sed dictum nibh non massa suscipit, non porta elit finibus. Nam id egestas eros, sed laoreet ligula. Nunc commodo est nec ante pellentesque, at rutrum libero laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras quis lacus at lectus consequat sodales.</p>
+                <p>Morbi porttitor velit eget mi viverra, ut elementum dolor porta. Duis pellentesque, purus quis sodales ullamcorper, orci neque commodo dui, sit amet volutpat dui nulla ac ante. Proin faucibus auctor mi non consequat. Suspendisse faucibus lorem sem, a interdum magna rutrum et. Suspendisse vulputate, mauris eget elementum viverra, enim magna egestas nulla, at suscipit neque nunc sit amet nisi. Nunc et dui eros. Aliquam ut nunc dictum ante fringilla interdum. Curabitur dapibus dolor eu sapien dignissim, id ornare turpis iaculis. Nam scelerisque finibus urna quis pharetra. Donec nec felis tortor.</p>
+                <p>Suspendisse ipsum nunc, posuere eget auctor a, pretium eget tortor. Duis hendrerit augue vel est auctor semper. Pellentesque ultrices euismod risus at imperdiet. Mauris viverra lorem commodo felis facilisis suscipit. Vivamus semper ex in dolor mollis, eu fringilla tortor molestie. Praesent ut leo non nisl egestas interdum et accumsan erat. Vivamus a quam nisl. Integer ornare leo libero, non interdum ex rhoncus et.</p>
+                <p>Aenean vitae metus euismod, consectetur lacus ac, sollicitudin nulla. Aliquam non ex in est egestas congue et sit amet urna. Maecenas gravida posuere eros at luctus. Sed laoreet sem non efficitur ultrices. Maecenas efficitur pretium ultrices. Suspendisse congue turpis sapien, sit amet pulvinar lacus vulputate hendrerit. Quisque rhoncus nulla non libero ornare venenatis. Pellentesque eget laoreet ligula. Phasellus at felis et nisl iaculis aliquam. Maecenas a mollis tellus, in porta enim. Aliquam nec euismod elit. Aenean quis odio ut ante vehicula vulputate. Suspendisse non ipsum sollicitudin, eleifend purus et, sollicitudin massa.</p>
+            </div>
+            <div class="w200 yellow oa">
+                <h2>news</h2>
+                <p>Pellentesque sapien nulla, rhoncus non dictum at, dignissim at purus. Aliquam erat volutpat. Maecenas eget lectus blandit, consequat ligula non, mattis risus. Cras pulvinar vulputate ultrices. Curabitur diam augue, rutrum tincidunt mauris id, egestas convallis arcu. Aenean feugiat ullamcorper nulla, eu rhoncus sem convallis sit amet. Vestibulum scelerisque, sem eget imperdiet porta, tortor justo egestas lacus, eu iaculis erat elit id ipsum. Donec luctus ante ac ornare ultricies. Fusce dapibus, nisi sed luctus eleifend, est ipsum pharetra felis, ut pellentesque risus nisl sodales arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer commodo at nibh commodo varius. Integer tempus vel justo sed feugiat.</p>
+                <p>Donec et ornare enim. Phasellus rutrum diam quis diam imperdiet sagittis. Fusce vel nisl sagittis, hendrerit tortor et, sollicitudin odio. Donec sodales quam eget turpis tincidunt, sodales aliquet massa porttitor. Curabitur eget gravida nibh. Cras nec urna vitae urna ullamcorper malesuada ac suscipit urna. Donec vulputate massa lorem, vel varius est iaculis id. Maecenas sollicitudin quis quam vitae viverra. Sed dictum nibh non massa suscipit, non porta elit finibus. Nam id egestas eros, sed laoreet ligula. Nunc commodo est nec ante pellentesque, at rutrum libero laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras quis lacus at lectus consequat sodales.</p>
+            </div>
+        </div>
+        <div>
+            <h2>footer</h2>
+        </div>
+    </div>
+
+</body>
+</html>
