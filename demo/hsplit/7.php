5ee09916b759c91743fd5bbe1c0738bb550f3cff
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2017-04-22 22:33:10 +0300

    add demo/hsplit/7

---
+++ demo/hsplit/7.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Title of the page</title>
+    <link href="../demo.css?<?php echo time() ?>" type="text/css" rel="stylesheet">
+</head>
+<body class="checkerboard">
+
+    <div class="fix tlbr vsplit oa">
+        <div class="grow max-h200 hsplit ma rainbow">
+            <div class="shrink mta">
+                <h2>left</h2>
+                <p>Integer luctus egestas quam, eget tincidunt justo varius vitae.</p>
+            </div>
+            <div class="shrink">
+                <h2>middle</h2>
+                <p>Integer luctus egestas quam, eget tincidunt justo varius vitae.</p>
+            </div>
+            <div class="shrink mta">
+                <h2>right</h2>
+                <p>Integer luctus egestas quam, eget tincidunt justo varius vitae.</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
