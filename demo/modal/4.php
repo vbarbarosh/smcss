508c30b0d88ee49c05ee87cc9433bad3ff01f742
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2017-06-06 15:55:24 +0300

    add modal/demo/4.php

---
+++ demo/modal/4.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Title of the page</title>
+    <link href="../demo.css?<?php echo uniqid() ?>" type="text/css" rel="stylesheet">
+</head>
+<body classx="no-scrollbars">
+
+    <div class="w500 ma mv50">
+        <p>Nullam convallis quam urna. Fusce et efficitur risus, at vulputate orci. Duis eget ante tincidunt, mattis enim non, sagittis libero. Aenean lacinia eros vel risus malesuada bibendum. Sed accumsan urna odio, feugiat aliquam mi feugiat non. Etiam vel est mi. Proin vitae dolor ut dolor consectetur mattis vitae varius dui. Duis non pulvinar ante. Vivamus et urna eget nulla dapibus suscipit vel quis felis. Vestibulum bibendum ornare nunc, eget condimentum nibh finibus nec. Sed erat purus, tempus commodo finibus in, mollis non purus. Curabitur ut sem ac quam aliquet scelerisque et eget diam.</p>
+        <p>Duis imperdiet feugiat diam, a egestas purus gravida sit amet. Quisque venenatis imperdiet justo eget tincidunt. Nulla et nibh ut orci gravida aliquet sit amet non est. Vivamus congue in turpis quis malesuada. Aenean vitae arcu pretium, ullamcorper metus eget, commodo tellus. Vestibulum mollis nisi et tellus pellentesque, at dictum ex eleifend. Proin fringilla tellus orci, eu congue dolor tincidunt sit amet. Sed in ornare sapien. Suspendisse nisi felis, dictum a tristique nec, varius tincidunt nulla. Vivamus dignissim dapibus dolor, eu tincidunt lacus vulputate non. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis faucibus mauris ac iaculis interdum. Integer tempor purus ut ligula egestas, id imperdiet eros lobortis.</p>
+        <p>Aenean a elit nibh. Phasellus nec interdum purus. Aenean quis dolor aliquam, gravida tellus vel, fermentum velit. Aliquam sed ex hendrerit, ultricies est id, placerat felis. In hac habitasse platea dictumst. Praesent egestas tristique pharetra. Sed eu nibh in ipsum efficitur tincidunt. Vestibulum placerat quis urna vel varius.</p>
+        <p>Cras non malesuada purus. Nunc tellus leo, pellentesque sed nibh in, gravida ultricies elit. Morbi placerat varius turpis. Phasellus sed nunc at nisl dignissim rhoncus at ac tortor. Curabitur egestas velit ex, at vulputate lorem vehicula non. Donec tellus sem, gravida non pulvinar pretium, malesuada et ligula. Nulla facilisi. In mollis consectetur arcu eu sodales. In dapibus lorem egestas felis pellentesque eleifend ac quis risus.</p>
+        <p>Praesent non suscipit lectus. Integer in lectus quam. Sed sed cursus mi. Maecenas mattis, metus ac volutpat vehicula, lacus felis vulputate est, at euismod tortor augue sed neque. Duis porttitor nisl et sapien efficitur, eget feugiat purus dapibus. Nam mattis aliquam felis, in tempor metus tempus at. In molestie vestibulum dolor ut consectetur.</p>
+        <p>Vestibulum aliquet enim quis sodales tincidunt. Donec vulputate magna diam, id feugiat nisi aliquam in. Cras gravida odio ac metus tincidunt pharetra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum porttitor laoreet neque vel tincidunt. Integer eu scelerisque purus, quis porta ligula. Donec molestie risus ac malesuada laoreet. Duis eleifend enim tincidunt sagittis efficitur. Nunc maximus libero eget rutrum sollicitudin. Praesent cursus non justo sed auctor. Phasellus nibh nibh, egestas eget leo quis, molestie varius felis. In lobortis porta ultrices. Nunc gravida semper diam, a tincidunt urna porta ut. Proin cursus mi quam, vitae lobortis nunc feugiat eu. Maecenas ut gravida lectus, eget tincidunt ante.</p>
+        <p>Etiam euismod nulla ultrices, gravida felis vitae, ultrices ipsum. Donec rutrum, tellus sit amet tincidunt posuere, enim ipsum pretium justo, sit amet malesuada nibh nulla ac diam. Morbi massa nunc, facilisis efficitur turpis sit amet, sodales consequat lectus. Maecenas felis arcu, mollis eu tincidunt id, lobortis sed lorem. Integer nisl ligula, malesuada sed odio vitae, blandit sodales velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis id diam fringilla, fringilla orci sit amet, fermentum quam. Suspendisse fringilla augue justo, a consequat urna pharetra ut. Nam ut purus in felis pharetra pretium non eget dolor. Nulla non magna a enim fermentum dictum et ut diam.</p>
+        <p>Vivamus mauris odio, lacinia vel eleifend et, tristique at eros. Maecenas fermentum diam mi, vitae venenatis purus feugiat nec. Phasellus dignissim mi in purus auctor, sed eleifend nisi auctor. Vestibulum eros eros, venenatis eget mattis eu, vestibulum eu enim. Sed ut velit vel eros rutrum porta. Sed sollicitudin nisi placerat leo pellentesque malesuada. Sed varius elit purus, id efficitur sapien finibus non. Ut volutpat, augue vitae ullamcorper mollis, arcu tortor efficitur ligula, sed malesuada ipsum metus quis enim. Nulla feugiat sapien ut dapibus tempus. Quisque maximus, sem vel imperdiet congue, lectus elit lobortis tortor, non sagittis ligula odio sed mauris. Aliquam vestibulum, quam ac dignissim ultricies, nisl lorem convallis arcu, sit amet cursus nisl orci at urna. Vestibulum vitae ligula ex. Morbi nunc augue, dignissim vel nunc ut, euismod iaculis ligula. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam tincidunt dignissim turpis a molestie.</p>
+        <p>Mauris euismod iaculis nisi sed ultrices. Pellentesque consectetur ullamcorper massa, vel ornare nulla lacinia eu. Maecenas ac dui leo. Ut eget auctor nisi. Sed faucibus vehicula mi ac feugiat. Nam ut sapien nibh. Vestibulum convallis neque id mollis feugiat. Nullam mattis id justo viverra feugiat. Mauris quis rutrum diam, ut tempor arcu. Donec imperdiet lacinia cursus. Vestibulum congue odio lorem. Vestibulum in tincidunt justo.</p>
+    </div>
+
+    <div class="fix tlbr oa flex-row" style="background: rgba(0, 0, 0, 0.25)">
+        <!--
+            * Firefox has no issues
+            * Chrome has no issues
+            * IE10 has no issues
+            * IE11 has no issues
+            * Edge14 has no issues
+            * Edge15 has no issues
+            * iOS10.1 has no issues
+            * iOS9.1 has no issues
+            * iOS8 has no issues
+            * iOS7 has no issues
+        -->
+        <div class="ma">
+            <div class="w500 ma mv15 white border bs15">
+                <!-- content -->
+                <p>Praesent non suscipit lectus. Integer in lectus quam. Sed sed cursus mi. Maecenas mattis, metus ac volutpat vehicula, lacus felis vulputate est, at euismod tortor augue sed neque. Duis porttitor nisl et sapien efficitur, eget feugiat purus dapibus. Nam mattis aliquam felis, in tempor metus tempus at. In molestie vestibulum dolor ut consectetur.</p>
+                <p>Vestibulum aliquet enim quis sodales tincidunt. Donec vulputate magna diam, id feugiat nisi aliquam in. Cras gravida odio ac metus tincidunt pharetra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum porttitor laoreet neque vel tincidunt. Integer eu scelerisque purus, quis porta ligula. Donec molestie risus ac malesuada laoreet. Duis eleifend enim tincidunt sagittis efficitur. Nunc maximus libero eget rutrum sollicitudin. Praesent cursus non justo sed auctor. Phasellus nibh nibh, egestas eget leo quis, molestie varius felis. In lobortis porta ultrices. Nunc gravida semper diam, a tincidunt urna porta ut. Proin cursus mi quam, vitae lobortis nunc feugiat eu. Maecenas ut gravida lectus, eget tincidunt ante.</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
