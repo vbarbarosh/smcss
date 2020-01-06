3280901de8723bd795a0f909ffbc7d774509c29f
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2020-01-06 12:23:54 +0200

    update src/docs/concepts/container.php

--- src/docs/concepts/container.php
+++ src/docs/concepts/container.php
@@ -4,4 +4,15 @@
 
     <p>Container &mdash; is a place for elements.</p>
 
+    <p>The only responsibility of containers is to perform layout.
+    They are expected to manage the following properties:</p>
+
+    <ul>
+        <li>position, top, left, right, bottom</li>
+        <li>width, height</li>
+        <li>margin, padding</li>
+    </ul>
+
+    <p>Only containers are responsible for managing this properties.</p>
+
 </div>
